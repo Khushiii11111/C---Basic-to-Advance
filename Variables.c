6232c3dac4c1bd7731c5d091e29408da5a78eaee
Khushi11111 <khushi.kushwaha.1211@gmail.com>
2026-06-20 23:31:36 +0530

    My Beginner Friendly C Code Files

---
+++ Variables.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+//We are learning variables. (Single Line Comment)
+
+    int number = 2;
+    printf("number is %d\n", number);
+
+    char star = '*';
+    printf("star looks like %c\n", star);
+
+    float pi = 3.14;
+    printf("value of pi is %f\n", pi);
+
+    int a = 50;
+    printf("a is %d\n", a);
+
+    int A = 70;
+    printf("A is %d\n", A);
+
+    int age = 11;
+    printf("age is %d\n", age);
+
+    int final_price = 150;
+    printf("price is %d\n", final_price);
+    return 0;
+}
+
+/*This is for multiline comments we need to put up in our project
+to understand how it actually works.
+We also undestood different types of variables: containers to store data.
+And Data Types like int, float, char as well, put into a single code file to differentiate them.*/
