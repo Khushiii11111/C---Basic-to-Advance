079d4feae25110433ff72b535cdd2546f61c0faa
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-22 23:00:12 +0530

    Area of square with float value

---
+++ PracticeProgram_2.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//area of square
+
+int main() {
+    int float;
+    printf("side: ");
+    scanf("%d", &side);
+
+    printf("area is: %f", side * side);
+
+    return 0;
+}
