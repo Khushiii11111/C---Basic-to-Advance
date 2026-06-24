5911adf0eeae8754db5c38f7e5c9e8ef028ca01d
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-24 14:33:31 +0530

    Area of Circle via C Programming

---
+++ PracticeProgram_2.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Area of Circle
+
+int main () {
+    float radius;
+    printf("enter radius: ");
+    scanf("%f", &radius);
+
+    printf("area is: %f", 3.14 * radius * radius);
+    return 0;
+}
