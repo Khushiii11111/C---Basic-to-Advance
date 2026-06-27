d2e61840a221d64891cd487625220e68478c99e1
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-27 20:17:01 +0530

    Area of Cube using a Float Value via C Programming

---
+++ PracticeProgram_2_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Area of Cube
+
+int main () {
+    float side;
+    printf("enter side: ");
+    scanf("%f", &side);
+
+    printf("area is: %f", 6 * side * side);
+    return 0;
+}
