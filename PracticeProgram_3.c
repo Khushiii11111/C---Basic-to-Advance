4b29ed4ec9a6654de7b4ba394efc6725eea911bf
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-29 18:19:32 +0530

    Cube of a Random Number n via C Programming

---
+++ PracticeProgram_3.c
@@ -0,0 +1,11 @@
+// Cube of a Random Number n
+
+#include <stdio.h>
+    int main() {
+        int n;
+        printf("enter n: ");
+        scanf("%d", &n);
+
+        printf("cube is: %d", n * n * n);
+        return 0;
+    }
