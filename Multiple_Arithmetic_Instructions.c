ce57e61e58474bd0b24191250cde9e0594330b8e
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-29 21:32:09 +0530

    Understading Mathematical Operators in C Programming

---
+++ Multiple_Arithmetic_Instructions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    int main() {
+
+        int a = 10, b = 2;
+
+        int sum = a + b;
+        int substraction = a - b;
+        int multiply = a * b;
+        int division = a/b;
+
+        printf("sum is: %d\n", sum);
+        printf("substraction is: %d\n", substraction);
+        printf("multiply is: %d\n", multiply);
+        printf("division is: %d\n", division); 
+
+        return 0;
+    }
