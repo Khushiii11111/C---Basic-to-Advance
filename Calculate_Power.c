bd53f2ab0a8824a4f6c1fc71d62e963fcd178d6f
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-29 21:51:52 +0530

    Power Calculation Function in C Programming

---
+++ Calculate_Power.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+    int main() {
+        int a, b;
+        a = 2, b = 3;
+        int power = pow(a,b);
+
+        printf("power of a: %d", power);
+        return 0;
+    }
