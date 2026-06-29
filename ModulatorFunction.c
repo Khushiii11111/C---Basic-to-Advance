993b82f29e22592932da6af1378d0e2c2b2be6bc
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-06-29 22:08:51 +0530

    UseCase: Function used to get Remainder of Integer Numbers in C Programming

---
+++ ModulatorFunction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+// Usecase: Understanding How to get a Remainder in C Programming using Modulator Function
+
+    int main() {
+        printf("Remainder is: %d", 16%3);
+        return 0;
+    }
