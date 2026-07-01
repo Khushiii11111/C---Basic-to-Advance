68f8e5e9248652375c80d591b7ea4af47dc3a669
Khushiii11111 <khushi.kushwaha.1211@gmail.com>
2026-07-01 20:12:16 +0530

    Associativity Rule in C Programming

---
+++ AssociativityPrecedence.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+// Associativity: Left to Right (For same Precedence)
+
+    int main() {
+        int a = 6 * 8 / 4 * 9;
+        printf("%d \n", a);
+        return 0;
+    }
